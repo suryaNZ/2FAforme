8759108c456dbcb32e3768d39b5df28c1f6b7482
suryaNZ <142939943+suryaNZ@users.noreply.github.com>
2025-09-02 20:08:53 +1200

    encryption / decryption works!

--- app/src/main/java/com/surya/a2faforme/2FAforme.kt
+++ app/src/main/java/com/surya/a2faforme/2FAforme.kt
@@ -9,13 +9,30 @@ import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 //import kotlin.io.encoding.Base64
 import android.util.Base64
+import android.util.Log
+import javax.crypto.spec.GCMParameterSpec
 
 private const val KEY_ALIAS = "TOTP_SECRET_KEY"
 
 class A2FAforme : Application() {
     override fun onCreate() {
         super.onCreate()
-        generateSecretKey()
+//        Log.d("TEST_2FA", "started oncreate")
+        val keyStore = KeyStore.getInstance("AndroidKeyStore").apply { load(null) }
+        while(!keyStore.containsAlias(KEY_ALIAS)) {
+            generateSecretKey()
+        }
+
+//        val original = "test_2fa_secret"
+//
+//        val (encrypted, iv) = encryptData(original)
+//        val decrypted = decryptData(encrypted, iv)
+//
+//        Log.d("TEST_2FA", "Original:  $original")
+//        Log.d("TEST_2FA", "Encrypted: $encrypted")
+//        Log.d("TEST_2FA", "IV:        $iv")
+//        Log.d("TEST_2FA", "Decrypted: $decrypted")
+
     }
 }
 
@@ -39,7 +56,7 @@ fun generateSecretKey() {
     keygen.generateKey()
 }
 
-fun ecryptData(plaintext:String): Pair<String, String> {
+fun encryptData(plaintext:String): Pair<String, String> {
     val keyStore = KeyStore.getInstance("AndroidKeyStore").apply {load(null)}
     val secretKeyEntry = keyStore.getEntry(KEY_ALIAS, null) as KeyStore.SecretKeyEntry
     val secretKey: SecretKey = secretKeyEntry.secretKey
@@ -54,4 +71,20 @@ fun ecryptData(plaintext:String): Pair<String, String> {
         Base64.encodeToString(ciphertext, Base64.DEFAULT),
         Base64.encodeToString(iv, Base64.DEFAULT)
     )
+}
+
+fun decryptData(ciphertextB64:String, ivB64:String): String {
+    val keyStore = KeyStore.getInstance("AndroidKeyStore").apply {load(null)}
+    val secretKeyEntry = keyStore.getEntry(KEY_ALIAS, null) as KeyStore.SecretKeyEntry
+    val secretKey: SecretKey = secretKeyEntry.secretKey
+
+    val iv = Base64.decode(ivB64, Base64.DEFAULT)
+    val ciphertext = Base64.decode(ciphertextB64, Base64.DEFAULT)
+
+    val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+    val spec = GCMParameterSpec(128, iv)
+    cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
+
+    val plaintextbytes = cipher.doFinal(ciphertext)
+    return String(plaintextbytes, Charsets.UTF_8)
 }
